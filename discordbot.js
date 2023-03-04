7fb188ed9c744203703017e8902c90a8d0b87072
avocadocoder73 <113757969+avocadocoder73@users.noreply.github.com>
2023-03-04 11:37:49 -0600

    first commit

--- discordbot.js
+++ discordbot.js
@@ -1,53 +1,60 @@
 require('dotenv').config()
-
 const finnhub = require('finnhub');
-
 const BOT_PREFIX = "/"
-
-
-
+const Discord = require('discord.js')
 const api_key = finnhub.ApiClient.instance.authentications['api_key'];
 api_key.apiKey = "cah7062ad3i2d29fju3g"
 const finnhubClient = new finnhub.DefaultApi()
 
-finnhubClient.quote("GOTU", (error, data, response) => {
-  
-  
-var price = data.c
+ 
 
-const alrt = "\d"
-
-const GOTU_COMMAND = "gotu"
-
-const Discord = require('discord.js')
 
 const client = new Discord.Client({
 
-    intents: ["GUILDS", "GUILD_MESSAGES"]
-    
+	intents: [
+
+		"GUILDS",
+		"GUILD_MESSAGES",
+	]
+})
+
+client.on('ready',() =>{
+
+	console.log('Ready!')
+
+})
+
+client.on("messageCreate", message =>{
+
+
+let stringcontainer = message.content
+
+let separate = stringcontainer.substring(stringcontainer.indexOf("-") + 1)
+
+
+
+if(message.content === '-' + separate)
+{
+	finnhubClient.quote(separate, (error, data, response) => {
+	let price = data.c
+
+	if(price != 0){
+
+	
+	message.reply("The price of "+ separate +" is $ " + price)
+	}
+	})
+
+}
 })
 
 
-client.on('ready', () => {
 
-    console.log('The bot is ready');
-})
 
-client.on("message", msg => {
-
-    if(msg.content === "-gotu")
-        msg.channel.send({embeds: ["The price of Gaotu Techedu is $" + price]});
-      
-    
-
-       
-
-        
-
-        
-});
 
 
 client.login(process.env.BOT_TOKEN)
 
-});
+
+	
+;
